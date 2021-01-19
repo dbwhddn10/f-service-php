260eea0d3870f9c26778abcdf1b7b980cb463043
dbwhddn10 <dbwhddn10@gmail.com>
2021-01-19 18:18:13 +0900

    add SoftDeleteService.php

---
+++ src/DB/Illuminate/SoftDeleteService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Dbwhddn10\FService\DB\Illuminate;
+
+use Dbwhddn10\FService\Service;
+use Dbwhddn10\FService\DB\Illuminate\Feature\ModelFeatureService;
+
+class SoftDeleteService extends Service
+{
+    public static function getArrBindNames()
+    {
+        return [];
+    }
+
+    public static function getArrCallbackLists()
+    {
+        return [
+            'result.model' => function ($model) {
+
+                $model->deleted_at = (new DateTime)->format('Y-m-d H:i:s');
+                $model->save();
+            },
+        ];
+    }
+
+    public static function getArrLoaders()
+    {
+        return [
+            'result' => function () {
+
+                return null;
+            },
+        ];
+    }
+
+    public static function getArrPromiseLists()
+    {
+        return [];
+    }
+
+    public static function getArrRuleLists()
+    {
+        return [];
+    }
+
+    public static function getArrTraits()
+    {
+        return [
+            ModelFeatureService::class,
+        ];
+    }
+}
